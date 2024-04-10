ee1ee9fd7f989ba3d27b5b134c1d5b2fe09a2e0d
vinhoMesquita <vinicius.albuquerque.etech@gmail.com>
2024-04-10 17:42:28 -0300

    substituição de letra na string

---
+++ nome_posicao_trocar_letra.py
@@ -0,0 +1,20 @@
+#FAÇA UM PROGRAMA E PYTHON QUE RECEBA O NOME DO USUARIO E A POSICAO EM QUE ELE DESEJA
+#ADICIONAR UMA LETRA, SE A LETRA QUE ESTIVER NA POSICAO INDICADA FOR R,
+#SUBSTITUIR POR S, SE FOR M SUBSTITUIR POR N
+#SENAO FOR NENHUMA DESSAS, REMOVER A LETRA DO NOM E IMPRIMA NA TELA
+#A PALAVRA REFORMULADA
+
+
+nome = str(input("Digite seu nome: "))
+
+i = int(input("Digite a posição que quer adicionar uma letra: "))
+
+
+if nome[i] == "R" or nome[i] == "r":
+   print(nome[:i]+"r"+nome[i+1:])
+elif nome[i] == "m" or nome[i] == "M":
+    print(nome[:i]+"n"+nome[i+1:])
+else: 
+   print(nome[:i]+"r"+nome[i+1:])
+
+
